d1733760c9fc3c60e6f17a3502b4508950a85540
dhjelmar <dlhjel@gmail.com>
2023-12-23 07:21:40 -0500

    added parameters to function call; changed to export all columns from map input file

--- modules/read_map.py
+++ modules/read_map.py
@@ -1,17 +1,16 @@
-def read_map():
+def read_map(file='input_files/map.xlsx', sheet='Sheet1'):
     ## pd.read_excel('fn.xlsx', sheet_name=0, header=2)
     import pandas as pd
     import sys
-    mapfile = 'input_files/map.xlsx'
-    print('map file:', mapfile)
+    print('map file:', file)
     
-    map = pd.read_excel(mapfile)
+    map = pd.read_excel(file, sheet_name=sheet)
     map['Account'] = map['Account'].str.strip()    # strip leading and trailing white space
     map['AccountNum'] = map.Account.str.extract('(^\d+a|^\d+)') # regular expression: numbers followed by a or just numbers
     
     ## only keep needed columns
     ## map = map[['InOrOut', 'Category', 'GreenSheet', 'Committee', 'SourceOfFunds', 'Account', 'AccountNum']]
-    map = map[['InOrOut', 'Category', 'SourceOfFunds', 'Account', 'AccountNum']]
+    ## map = map[['InOrOut', 'Category', 'SourceOfFunds', 'Account', 'AccountNum', 'Purpose', 'InternalExternal']]
     ## print(map.head())
 
     # check for non-unique account numbers
